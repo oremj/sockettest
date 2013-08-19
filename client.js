5480d41fa11c19a36cd61fb4a63e80e223f76d18
Jeremiah Orem <oremj@oremj.com>
2013-08-19 10:57:44 -0700

    Clients

--- client.js
+++ client.js
@@ -1,5 +1,8 @@
 var net = require('net');
 
+var PORTS=20;
+var CLIENTS=50000;
+
 var client;
 
 var connected = 0;
@@ -12,8 +15,9 @@ setInterval(function() {
     console.log("%d connected", connected);
 }, 2500);
 
-for(var i=0; i < 20; i++) {
-    for(var j=0; j < 20; j++) {
+for(var i=0; i < PORTS; i++) {
+    console.log("%d connected", i * CLIENTS);
+    for(var j=0; j < CLIENTS; j++) {
         net.connect({port: 9000 + i}, connect);
     }
 }
